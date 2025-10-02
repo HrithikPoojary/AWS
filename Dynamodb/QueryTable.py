7981fdb544cb30c9723e4bce15b9cdb7cfe0c4dc
Hrithik <learningdemo00@gmail.com>
2025-10-03 01:20:22 +0530

    Expression

---
+++ Dynamodb/QueryTable.py
@@ -0,0 +1,18 @@
+import boto3 #type:ignore
+from boto3.dynamodb.conditions import Key #type:ignore
+
+dynamodb = boto3.resource(
+        'dynamodb',
+        endpoint_url = 'http://localhost:8000',
+        region_name = 'us-west-2a',
+        aws_access_key_id = 'fakeMyKeyId',
+        aws_secret_access_key = 'fakeSecretAccessKey'
+)
+
+table = dynamodb.Table("Product")
+response = table.query(
+        KeyConditionExpression=Key('Product_id').eq('p001')
+)
+print('Print only electronic items')
+for item in response['Items']:
+        print(item)
