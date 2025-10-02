0864a872414fbc75859d8bf85d96722d7f5e25ca
Hrithik <learningdemo00@gmail.com>
2025-10-03 00:59:11 +0530

    Inserted 2 rows

---
+++ Dynamodb/InsertingTable1.py
@@ -0,0 +1,28 @@
+import boto3 #type:ignore
+
+dynamodb = boto3.resource(
+        'dynamodb',
+        endpoint_url = 'http://localhost:8000',
+        region_name = 'us-east-2a',
+        aws_access_key_id = 'fakeMyKeyId',
+        aws_secret_access_key = 'fakeSecretAccessKey'
+)
+
+table = dynamodb.Table('Product')
+
+with table.batch_writer() as batch:
+        batch.put_item(
+                Item = {
+                        'Product_id' : 'p004',
+                        'Category' : 'Electronics',
+                        'Name' : 'Vehicale'
+                }
+        )
+        batch.put_item(
+                Item = {
+                        'Product_id' : 'p005',
+                        'Category' : 'Electronics',  
+                        'Name':'Laptop'
+                }
+        )
+print('Inserted 3 items')
